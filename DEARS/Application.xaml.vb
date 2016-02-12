5af04b1d436cf17631af835d91dc9e000f8f6e3d
Mohanad Ahmed <mohanadahmed@windowslive.com>
2016-02-12 16:20:09 +0300

    Added Reference to System.Windows.Threading to get DispatcherUnhandledExceptionEventArgs defined hope itworks!!

--- DEARS/Application.xaml.vb
+++ DEARS/Application.xaml.vb
@@ -1,9 +1,10 @@
-﻿Class Application
+﻿Imports System.Windows.Threading
+Class Application
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
 
-    
+
     Private Sub Application_DispatcherUnhandledException(sender As Object, e As Windows.Threading.DispatcherUnhandledExceptionEventArgs) Handles Me.DispatcherUnhandledException
         MsgBox(FlattenOutException(e.Exception))
         e.Handled = True
